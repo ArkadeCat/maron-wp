86121f9a6791d6f99d1e3b31623686b7fca7ccff
Héktor Peña <dev@hektor.mx>
2019-02-22 11:44:00 -0600

    Start

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+jQuery('document').ready(function($){
+    $('.navbar-burger').click(function() {
+        $('.navbar-burger').toggleClass('is-active');
+        $('.navbar-menu').toggleClass('is-active');
+      });
+    
+});
+
+
+
+
